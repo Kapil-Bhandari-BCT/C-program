bf65e438f914e8bc45a2db4c5e77ee1d5657d0b7
Kapil Bhandari <pas081bct017@wrc.edu.np>
2025-02-06 21:45:54 +0545

    Add files via upload

---
+++ Sphere.cpp
@@ -0,0 +1,23 @@
+// C program to calculate the surface Area and volume of Sphere
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float radius;
+
+    float volume;
+    float area;
+
+    printf("Enter the radius: ");
+    scanf("%f", &radius);
+
+    volume = (4.0 / 3) * (3.14) * pow(radius, 3);
+    area = 4 * (3.14) * pow(radius, 2);
+
+    printf("Volume of Sphere 	  : %f\n", volume);
+    printf("Surface area of Sphere: %f\n", area);
+
+    return 0;
+}
