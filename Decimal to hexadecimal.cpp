0c327a5703c12c50a37845b743fbd1ff9d993ac9
Kapil Bhandari <pas081bct017@wrc.edu.np>
2025-02-06 21:45:16 +0545

    Add files via upload

---
+++ Decimal to hexadecimal.cpp
@@ -0,0 +1,12 @@
+/*C program to convert decimal to hexadecimal*/
+
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter a number: ");
+	scanf("%d",&n);
+	
+	printf("The hexadecimal equivalent of %d is %X",n,n);
+
+}
