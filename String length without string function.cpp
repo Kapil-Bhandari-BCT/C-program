23f9edf4f52ad219f37d18107c4e0e3668c1b8c3
Kapil Bhandari <pas081bct017@wrc.edu.np>
2025-02-07 20:58:04 +0545

    Add files via upload

---
+++ String length without string function.cpp
@@ -0,0 +1,18 @@
+//C program to find string length without string function
+
+#include<stdio.h>
+
+int main()
+{
+	int l=0,i;
+	char str[100];
+	printf("Enter a string: ");
+	gets(str);
+	
+	for(i=0;str[i]!='\0';i++)
+	{
+		l++;
+	}
+	printf("The length of the string is %d",l);
+	return 0;
+}
