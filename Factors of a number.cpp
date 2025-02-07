30e8190dcc27de8910aa807a945d956228f7d9d4
Kapil Bhandari <pas081bct017@wrc.edu.np>
2025-02-07 20:56:17 +0545

    Add files via upload

---
+++ Factors of a number.cpp
@@ -0,0 +1,17 @@
+//C program to print factors of a number
+
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("Enter a number: ");
+	scanf("%d",&n);
+	printf("The factors of %d are: ",n);
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			printf("%d, ",i);
+		}
+	}
+}
