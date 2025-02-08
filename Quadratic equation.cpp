34d18644dea232d0ff571eb862caeebacf02a590
Kapil Bhandari <pas081bct017@wrc.edu.np>
2025-02-08 21:15:20 +0545

    Add files via upload

---
+++ Quadratic equation.cpp
@@ -0,0 +1,63 @@
+//C program to roots of a quadratic equation
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void calculateRoot(float a, float b, float c)
+{
+    float rootA ;
+    float rootB;
+
+    float realp;
+    float imagp ;
+    float disc;
+    if (a == 0 || b == 0 || c == 0) {
+        printf("Error: Unable to determine roots\n");
+        return;
+    }
+    else {
+        disc = b * b - 4.0 * a * c;
+        if (disc < 0) {
+            printf("Imaginary Roots\n");
+            realp = -b / (2.0 * a);
+            imagp = sqrt(abs(disc)) / (2.0 * a);
+            printf("Root1 = %f  +i %f\n", realp, imagp);
+            printf("Root2 = %f  -i %f\n", realp, imagp);
+        }
+        else if (disc > 0) {
+            printf("Roots are real and distinct \n");
+            rootA = (-b + sqrt(disc)) / (2.0 * a);
+            rootB = (-b - sqrt(disc)) / (2.0 * a);
+            printf("Root1 = %f  \n", rootA);
+            printf("Root2 = %f  \n", rootB);
+        }
+        else if (disc == 0) {
+            printf("Roots are real and equal\n");
+            rootA = -b / (2.0 * a);
+            rootB = rootA;
+            printf("Root1 = %f\n", rootA);
+            printf("Root2 = %f\n", rootB);
+        }
+    }
+}
+
+int main()
+{
+    float a;
+    float b;
+    float c;
+
+    printf("Enter the values of a: ");
+    scanf("%f", &a);
+
+    printf("Enter the values of b: ");
+    scanf("%f", &b);
+
+    printf("Enter the values of c: ");
+    scanf("%f", &c);
+
+    calculateRoot(a, b, c);
+
+    return 0;
+}
