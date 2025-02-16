345d11f04aeffa6ae4de5a0d2600d237d22324fb
Kapil Bhandari <pas081bct017@wrc.edu.np>
2025-02-16 22:34:49 +0545

    Add files via upload

---
+++ Area of cube.cpp
@@ -0,0 +1,11 @@
+//C program to calculate the area of Cube
+#include <stdio.h>
+int main()
+{
+    float side, area;
+    printf("Enter the side of the cube: ");
+    scanf("%f", &side);
+    area = 6 * side * side;
+    printf("Area of the cube: %.2f\n", area);
+    return 0;
+}
